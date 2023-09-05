ce9aab6ca97bd2ffcaea492fe80f63ac78c35230
aniruddha <ani@dctacademy.com>
2023-09-05 13:01:06 +0530

    list api projects

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express() 
 const port = 3099
 
+app.get('/api/projects', (req, res) => {
+    const projects = ['project 1', 'project 2', 'project 3']
+    res.json(projects) 
+})
+
 app.listen(port, () => {
     console.log('running on port', port)
 })
